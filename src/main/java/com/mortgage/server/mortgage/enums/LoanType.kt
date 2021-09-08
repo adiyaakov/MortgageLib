878f163b14f05ea8883ae508bc6ac3f98f3b4943
Adi Yaakov <adi@bambidynamic.com>
2021-09-08 08:21:58 +0300

    [LoanType] Add id index

--- src/main/java/com/mortgage/server/mortgage/enums/LoanType.kt
+++ src/main/java/com/mortgage/server/mortgage/enums/LoanType.kt
@@ -1,5 +1,6 @@
 package com.mortgage.server.mortgage.enums
 
-enum class LoanType {
-    FIX_RATE, STICKY_FIX_RATE, FIX_RATE_FIVE_YEARS, STICKY_FIX_RATE_FIVE_YEARS , PRIME;
+enum class LoanType(val index: Int) {
+    FIX_RATE(0), STICKY_FIX_RATE(1), FIX_RATE_FIVE_YEARS(2), STICKY_FIX_RATE_FIVE_YEARS(3) , PRIME(4);
 }
+
