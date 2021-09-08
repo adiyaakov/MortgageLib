7aad80512c84312fa6ee56158bc255707dab0e37
Adi Yaakov <adi@bambidynamic.com>
2021-09-08 07:26:28 +0300

    []

--- src/main/java/com/mortgage/server/mortgage/MortgageApplication.java
+++ src/main/java/com/mortgage/server/mortgage/MortgageApplication.java
@@ -7,17 +7,6 @@ import com.mortgage.server.mortgage.models.Mortgage;
 public class MortgageApplication {
 
 	public static void main(String[] args) {
-		Mortgage mortgage = new Mortgage(1000000, 200000, 5000);
-		mortgage.insertOrUpdateLoan(LoanType.FIX_RATE, new FixedRate());
-
-		mortgage.getLoansMix().get(LoanType.FIX_RATE).getFirstPayment();
-
-		mortgage.getLoansMix().get(LoanType.FIX_RATE).setPrinciple(230000);
-		mortgage.getLoansMix().get(LoanType.FIX_RATE).setRate(2.3);
-		mortgage.getLoansMix().get(LoanType.FIX_RATE).setYearsLength(20);
-
-		mortgage.getLoansMix().get(LoanType.FIX_RATE).getFirstPayment();
-
 
 	}
 
