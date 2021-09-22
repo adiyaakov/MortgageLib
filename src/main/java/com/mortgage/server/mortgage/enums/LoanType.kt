33ecebc2210eecec4cb1675f8ac596faf2684626
Adi Yaakov <adi@bambidynamic.com>
2021-09-22 14:30:08 +0300

    [tras]

--- src/main/java/com/mortgage/server/mortgage/enums/LoanType.kt
+++ src/main/java/com/mortgage/server/mortgage/enums/LoanType.kt
@@ -2,5 +2,25 @@ package com.mortgage.server.mortgage.enums
 
 enum class LoanType(val index: Int) {
     FIX_RATE(0), STICKY_FIX_RATE(1), FIX_RATE_FIVE_YEARS(2), STICKY_FIX_RATE_FIVE_YEARS(3) , PRIME(4);
+
+    fun hebName() : String {
+        return when(this) {
+            FIX_RATE -> "קבועה לא צמודה"
+            STICKY_FIX_RATE -> "קבועה צמודה"
+            FIX_RATE_FIVE_YEARS -> "קבועה לא צמודה משתנה כל 5 שנים"
+            STICKY_FIX_RATE_FIVE_YEARS -> "קבועה צמודה משתנה כל 5 שנים"
+            PRIME -> "פריים"
+        }
+    }
+
+    fun hebShortName() : String {
+        return when(this) {
+            FIX_RATE -> "קל'צ"
+            STICKY_FIX_RATE -> "ק'צ"
+            FIX_RATE_FIVE_YEARS -> "קל'צ 5 שנים"
+            STICKY_FIX_RATE_FIVE_YEARS -> "ק'צ 5 שנים"
+            PRIME -> "פריים"
+        }
+    }
 }
 
